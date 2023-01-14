f48632300c44e4ff331056cf0afdaa460011c90d
rayfrye <rfrye09@gmail.com>
2023-01-14 13:59:34 -0500

    Initial Setup
    Initial Setup

---
+++ FT.Data_Generation_Orchestrator/Program.cs
@@ -0,0 +1,11 @@
+﻿using FT.Data_Generation_Orchestrator;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
+
