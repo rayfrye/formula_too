bce59ebdc0beadde50180e9434bb06fd54c80220
rayfrye <rfrye09@gmail.com>
2023-01-15 22:05:33 -0500

    switched to api
    using an api instead of a worker service.

---
+++ FT.DataGenerator/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FT.DataGenerator.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> log;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> _log)
+    {
+        log = _log;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+
+        log.LogInformation("Test");
+
+        return forecasts;
+    }
+}
+
---
+++ FT.DataGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Use Serilog
+builder.Host.UseSerilog((hostContext, services, configuration) => {
+    configuration
+        .WriteTo.File("serilog-file.txt")
+        .WriteTo.Console();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
